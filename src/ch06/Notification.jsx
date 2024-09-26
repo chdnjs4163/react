8f6866a46ff36a3a02293e1f14285fc6dbd74e40
chdnjs4163 <162400165+chdnjs4163@users.noreply.github.com>
2024-09-26 16:31:35 +0900

    Add files via upload

---
+++ src/ch06/Notification.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Notification.css"
+// const styles =   {
+//     wrapper:{
+//         margin: 8,
+//         padding: 8,
+//         display: "flex",
+//         flexDirection: "row",
+//         border: "1px solid grey",
+//         borderRadius: 16,
+//     },
+//     messageText: {
+//         color:"magenta",
+//         fontsize: 16,
+//     },
+// };
+
+class Notification extends React.Component{
+
+    constructor(props) {
+        super(props); //Component가 생성될 떄 props를 전달받을 수 있는 매개변수를 갖는 생성자가 호출된다.
+
+        this.state = {
+
+        };
+    }
+
+    componentDidMount() {
+        console.log(`${this.props.id} : componentDidMount() called`);
+    }
+
+    componentDidUpdate( ) {
+        console.log(`${this.props.id} componentDidUpdate() called`);
+    }
+
+    componentWillUnmount() {
+        console.log(`${this.props.id} componentWillUnmount() called`);
+    }
+
+    render(){
+        return(
+            <div className="wrapper">
+                <span className="messageText">
+                    {this.props.message}
+                </span>
+            </div>
+        );
+    }
+}
+
+export default Notification;
